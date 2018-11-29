678ed003f6f1f31484c4c9b4deeddd0852e5dad8
xin <18342208462@163.com>
2018-11-29 14:42:57 +0800

    release

--- src/main/java/com/hand/test/controller/LoginController.java
+++ src/main/java/com/hand/test/controller/LoginController.java
@@ -3,6 +3,10 @@ package com.hand.test.controller;
 import com.hand.test.dto.SysUser;
 import com.hand.test.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,17 +19,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
     @Autowired
-    private SysUserService sysUserService;
+    private AuthenticationManager authenticationManager;
 
     @RequestMapping("/loginAuth")
     public String login(SysUser sysUser){
-        Integer num=sysUserService.selectByName(sysUser);
-        if(num>0){
-            SysUser user=sysUserService.selectUser(sysUser);
-            if(user!=null){
-                return "employee";
-            }
+        try {
+            // 用户提交的username和password
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(sysUser.getUsername(), sysUser.getPassword());
+
+            // 进行认证，【给方法会调用loadUserByUsername并返回UserDetais，并与用户的提交进行比对】
+            Authentication authentication = authenticationManager.authenticate(authRequest);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return "employee";
+
+        }catch (Exception e){
+            return "redirect:/login.html";
         }
-        return "redirect:/login.html";
     }
 }
